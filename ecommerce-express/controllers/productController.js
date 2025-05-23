c246f46ef86de045600d6af1f56a4fc572a4a38e
martinolanza03 <lanzamartino3@gmail.com>
2025-05-23 13:05:38 +0200

    feat: add crud Update  *work in progress*

--- ecommerce-express/controllers/productController.js
+++ ecommerce-express/controllers/productController.js
@@ -2,32 +2,34 @@
 const connection = require("../data/db");
 
 const index = (req, res) => {
-    const sql = "SELECT * FROM products";
-    connection.query(sql, (error, result) => {
-      if (error) {
-        return res.status(500).json({msg: "Errore del database", code: 500});
-      } else if (result.length === 0) {
-        return res.status(404).json({msg: "Non è stato possibile trovare risultati", code: 404});
-      }
-      return res.status(200).json({msg: "Benvenuto nell' API", code: 200, products: result});
-    })
+  const sql = "SELECT * FROM products";
+  connection.query(sql, (error, result) => {
+    if (error) {
+      return res.status(500).json({ msg: "Errore del database", code: 500 });
+    }
+    if (result.length === 0) {
+      return res.status(404).json({ msg: "Non è stato possibile trovare risultati", code: 404 });
+    }
+    return res.status(200).json({ msg: "Benvenuto nell' API", code: 200, products: result });
+  })
 }
 
 
 const show = (req, res) => {
-    const { id } = req.params;
-    const sql = "SELECT * FROM products WHERE id = ?";
-    connection.query(sql, [id], (error, result) => {
-      if (error) {
-        return res.status(500).json({msg: "Errore del database", code: 500});
-      } else if (result.length === 0) {
-        return res.status(404).json({msg: "Non è stato possibile trovare risultati", code: 404});
-      }
-      return res.status(200).json({msg: "Benvenuto nell' API", code: 200, products: result});
-    })
+  const { id } = req.params;
+  const sql = "SELECT * FROM products WHERE id = ?";
+  connection.query(sql, [id], (error, result) => {
+    if (error) {
+      return res.status(500).json({ msg: "Errore del database", code: 500 });
+    }
+    if (result.length === 0) {
+      return res.status(404).json({ msg: "Non è stato possibile trovare risultati", code: 404 });
+    }
+    return res.status(200).json({ msg: "Benvenuto nell' API", code: 200, products: result });
+  })
 }
 
 module.exports = {
-    index,
-    show,
+  index,
+  show,
 }
--- ecommerce-express/routers/orderRouter.js
+++ ecommerce-express/routers/orderRouter.js
@@ -1,11 +1,11 @@
 
-const { index, store, show } = require("../controllers/orderController");
+const { index, show, update } = require("../controllers/orderController");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", index);
-router.post("/", store);
 router.get("/:id", show);
+router.patch("/:id", update)
 
 
 module.exports = router;
