5f170231eec702d0e4b1707a20ae0853d6ec044a
simoneburrai <burrai.s98@gmail.com>
2025-05-26 13:05:22 +0200

    feat: added controls on checkoutProcess

--- ecommerce-express/controllers/orderController.js
+++ ecommerce-express/controllers/orderController.js
@@ -1,4 +1,5 @@
-
+import countries from "../data/countries"
+const validator = require("validator")
 const connection = require("../data/db");
 
 // const index = (req, res) => {
@@ -33,13 +34,61 @@ const checkoutProcess = (req, res) => {
   const { name, surname, email, billing_address, shipping_address, phone, country, amount } = req.body;
   const sql = "INSERT INTO customers (`name`, `surname`, `email`, `billing_address`, `shipping_address`, `phone`, `country`) VALUES (?, ?, ?, ?, ?, ?, ?);"
 
+ //Validazioni
+//NOme e Cognome
+  if (!name || !surname) {
+    return res.status(400).json({ msg: "Insert a valid name and surname", code: 400 });
+  }
+
+ //Email controllo con validator
+  if (!email || !validator.isEmail(email)) {
+    return res.status(400).json({ msg: "Mail Error: Formato email non valido.", code: 400 });
+  }
+
+//Telefono: controllo lunghezza e esistenza
+const parsedPhone = parseInt(phone)
+  if (!phone || phone.length < 7 || phone.length > 20 || isNaN(parsedPhone)) {
+    return res.status(400).json({ msg: "Number Error: Phone number must be valid and respect standard format (7-20 digits).", code: 400 });
+  }
+
+  // Amount control
+  const parsedAmount = parseFloat(amount);
+  if (isNaN(parsedAmount) || parsedAmount < 0) {
+    return res.status(400).json({ msg: "Amount Error: You have to provide a valid positive price", code: 400 });
+  }
+
+  //controllo billing address
+
+   if (!billing_address || typeof billing_address !== 'string' || validator.isEmpty(billing_address.trim())) {
+        return res.status(400).json({ msg: "Billing Address: Insert a Valid Address", code: 400 }); // L'indirizzo non deve essere vuoto o solo spazi bianchi
+    }
+
+  // Lunghezza minima e massima ragionevole
+    if (!validator.isLength(billing_address, { min: 10, max: 200 })) { // Esempio: 10 a 200 caratteri
+        res.status(400).json({ msg: "Billing Address: Insert a Valid Address", code: 400 });
+    }
+
+  //controllo shipping_adress
+
+   if (!shipping_address || typeof shipping_address !== 'string' || validator.isEmpty(shipping_address.trim())) {
+        res.status(400).json({ msg: "Shipping Address: Insert a Valid Address", code: 400 });
+    }
+
+  // Lunghezza minima e massima ragionevole
+    if (!validator.isLength(shipping_address, { min: 10, max: 200 })) { // Esempio: 10 a 200 caratteri
+        res.status(400).json({ msg: "Shipping Address: Insert a Valid Address", code: 400 });
+    }
+
+  // Country control (array con countries importato from data)
+  if (!country || !countries.includes(country.toLowerCase())) {
+    return res.status(400).json({ msg: `Country Error: "${country}" is not a valid country`, code: 400 });
+  }
+  
+  
   connection.query(sql, [name, surname, email, billing_address, shipping_address, phone, country], (error, result) => {
     if (error) {
       console.log(error);
       return res.status(500).json({ msg: "Errore del database", code: 500 });
-    }
-    if (result.length === 0) {
-      return res.status(404).json({ msg: "Non è stato possibile trovare risultati", code: 404 });
     }
 
     const sql2 = `SELECT * FROM customers WHERE customers.name = ? AND customers.surname = ? AND customers.email = ?`
