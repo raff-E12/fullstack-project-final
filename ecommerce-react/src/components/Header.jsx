86d3e8ae3ba6aea39a662d0abc055447d25d63cb
Raff_12 <raffelecoppola381@gmail.com>
2025-05-27 20:31:52 +0200

    feat: Modifica layout...

--- ecommerce-react/src/components/Header.jsx
+++ ecommerce-react/src/components/Header.jsx
@@ -3,26 +3,27 @@ import { Link, NavLink } from "react-router-dom";
 export default function Header() {
     return(
      <>
-     <header className="header-sc">
+     <header className="header-sc container-fluid">
         <nav className="navbar">
-            <div className="nav-logo">VENDOR</div>
-            <ul className="nav-menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Pages</a></li>
-            <li><a href="#">Docs</a></li>
-            </ul>
+             <div className="nav-actions">
+                <ul className="nav-menu">
+                    <li><Link to={"/"}>Homepage</Link></li>
+                    <li><Link to={"/products"}>Products</Link></li>
+                    <li><Link to={"/categories"}>Categories</Link></li>
+                </ul>
+             </div>
+
+            <div className="nav-logo">Demo</div>
+
             <div className="nav-actions">
-            <a href="#">Login</a>
-            <a href="#" class="cart">Cart (2)</a>
+                <ul className="nav-menu">
+                  <li><Link to={"/checkout"}>Checkout</Link></li>
+                  <li><Link to={"/cart"}>Cart</Link></li>
+                  <li><Link to={"/"}>Login</Link></li>
+                </ul>
             </div>
       </nav>
      </header>
      </>
     )
 }
-
-
-                // <li><Link to={"/"}>Homepage</Link></li>
-                // <li><Link to={"/products"}>Products</Link></li>
-                // <li><Link to={"/categories"}>Categories</Link></li>
-                // <li><Link to={"/checkout"}>Checkout</Link></li>
