952336a4f17a6300e8d01a63793ce64fba98c29c
AndreaCatapano <a.catapano96@gmail.com>
2025-05-27 19:15:42 +0200

    feat: created header with bootstrap and no css

--- ecommerce-react/components/Header.jsx
+++ ecommerce-react/components/Header.jsx
@@ -1,34 +1,101 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header() {
-    return <header>
-        <div>Sono l'header</div>
-        <ul className="nav">
-            <li className="nav-item fw-bold">
-                <a className="nav-link" href="#">
-                    <img src="../../public/th.jpeg" className="logo" />
-                </a>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/" className="nav-link active fw-bold fw-header" aria-current="page">
-                    Homepage
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/products" className="nav-link fw-bold fw-header">
-                    Products
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/categories" className="nav-link fw-bold fw-header">
-                    Categories
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/checkout" className="nav-link fw-bold fw-header">
-                    Checkout
-                </NavLink>
-            </li>
-        </ul>
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <header className="py-3">
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="d-flex">
+            <NavLink
+              to="/"
+              className="nav-link fw-bold me-3"
+              aria-current="page"
+            >
+              Homepage
+            </NavLink>
+
+            <div className="dropdown">
+              <button
+                className="btn btn-link nav-link fw-bold p-0 dropdown-toggle"
+                type="button"
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              >
+                Categories
+              </button>
+              <div className={`dropdown-menu ${isDropdownOpen ? "show" : ""}`}>
+                <div className="d-md-flex">
+                  <div className="col-md-6">
+                    <NavLink
+                      className="dropdown-item"
+                      to="/category/polo-&-t-shirt"
+                      onClick={() => setIsDropdownOpen(false)}
+                    >
+                      Polo & t-Shirt
+                    </NavLink>
+                    <hr className="d-md-none my-2" />
+                    <NavLink
+                      className="dropdown-item"
+                      to="/category/capispalla"
+                      onClick={() => setIsDropdownOpen(false)}
+                    >
+                      Capispalla
+                    </NavLink>
+                    <hr className="d-md-none my-2" />
+                    <NavLink
+                      className="dropdown-item"
+                      to="/category/felpe"
+                      onClick={() => setIsDropdownOpen(false)}
+                    >
+                      Felpe
+                    </NavLink>
+                  </div>
+                  <div className="vr d-none d-md-block mx-2"></div>
+                  <hr className="d-md-none my-2" />
+                  <div className="col-md-6">
+                    <NavLink
+                      className="dropdown-item"
+                      to="/category/pantaloni"
+                      onClick={() => setIsDropdownOpen(false)}
+                    >
+                      Pantaloni
+                    </NavLink>
+                    <hr className="d-md-none my-2" />
+                    <NavLink
+                      className="dropdown-item"
+                      to="/category/Scarpe"
+                      onClick={() => setIsDropdownOpen(false)}
+                    >
+                      Scarpe
+                    </NavLink>
+                    <hr className="d-md-none my-2" />
+                    <NavLink
+                      className="dropdown-item"
+                      to="/category/Streetwear"
+                      onClick={() => setIsDropdownOpen(false)}
+                    >
+                      Streetwear
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <h1 className="m-0">Nome</h1>
+
+          <div className="d-flex">
+            <NavLink to="/products" className="nav-link fw-bold me-3">
+              Product
+            </NavLink>
+            <NavLink to="/cart" className="nav-link fw-bold">
+              Carrello
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </header>
+  );
 }
