5a981879943c9e7db55f25bb0a93139d7552a37e
Raff_12 <raffelecoppola381@gmail.com>
2025-05-27 16:24:01 +0200

    feat: Modifica Rotte File..

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DefaultLayout from '../layout/DefaultLayout';
-import CartPage from '../pages/CartPage';
-import CategoriesPage from '../pages/CategoriesPage';
-import CheckOutPage from '../pages/CheckOutPage';
-import Homepage from '../pages/Homepage';
-import ProductsPage from '../pages/ProductsPage';
-import SingleCategoryPage from '../pages/SingleCategoryPage';
-import SingleCategoryProductPage from '../pages/SingleCategoryProductPage';
-import SingleOrderPage from '../pages/SingleCategoryPage'
-import SingleProductPage from '../pages/SingleProductPage';
+import DefaultLayout from './layout/DefaultLayout';
+import CartPage from './pages/CartPage';
+import CategoriesPage from './pages/CategoriesPage';
+import CheckOutPage from './pages/CheckOutPage';
+import Homepage from './pages/Homepage';
+import ProductsPage from './pages/ProductsPage';
+import SingleCategoryPage from './pages/SingleCategoryPage';
+import SingleCategoryProductPage from './pages/SingleCategoryProductPage';
+import SingleOrderPage from './pages/SingleCategoryPage'
+import SingleProductPage from './pages/SingleProductPage';
 
 export default function App() {
   return <div>
