fe53f738bd3fdf2b1f9805d00c6982c4336c754c
willymariino <wg.mariino@gmail.com>
2025-05-28 13:31:45 +0200

    styling text

--- ecommerce-react/src/components/hero-cards.jsx
+++ ecommerce-react/src/components/hero-cards.jsx
@@ -6,7 +6,7 @@ function Hero() {
         <>
             <div className={styles.title}>
                 <h1>Le nostre collezioni</h1>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse itaque consequuntur culpa, obcaecati qui, at est pariatur, ipsa dicta laboriosam cupiditate quam natus perferendis iste incidunt alias molestias corporis fugit.</p>
+                <p className={styles.text}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse itaque consequuntur culpa, obcaecati qui, at est pariatur, ipsa dicta laboriosam cupiditate quam natus perferendis iste incidunt alias molestias corporis fugit.</p>
 
             </div>
 
