bcd1bc9ae84ae82caa3a876903366abdfd882934
AndreaCatapano <a.catapano96@gmail.com>
2025-05-28 19:02:26 +0200

    fix: corrected bug

--- ecommerce-react/src/pages/Homepage.jsx
+++ ecommerce-react/src/pages/Homepage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Sliders from "../components/Sliders";
 import HeroCard from "../components/HeroCard";
-import Sliders from "../components/Sliders";
 import CardSections from "../components-layouts/CardSections";
 
 export default function Homepage() {
