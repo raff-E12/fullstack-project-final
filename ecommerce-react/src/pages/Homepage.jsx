d7ceecd7a03bc6c442a4dd9c82749f0a9da80127
willymariino <wg.mariino@gmail.com>
2025-05-28 12:48:28 +0200

    created scaffolding hero card

--- ecommerce-react/src/pages/Homepage.jsx
+++ ecommerce-react/src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sliders from '../components/Sliders'
+import Hero from '../components/hero-cards'
 
 export default function Homepage() {
   return (
     <div className='container-xxl p-0'>
-        <Sliders />
+      <Sliders />
+      <Hero />
+
     </div>
   )
 }
