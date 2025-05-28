9e6cfb76a93a263673d0fb72903a44290e49cf11
martinolanza03 <lanzamartino3@gmail.com>
2025-05-28 17:18:23 +0200

    fix: fix cards with key and className

--- ecommerce-react/src/components/Cards.jsx
+++ ecommerce-react/src/components/Cards.jsx
@@ -4,16 +4,15 @@ import ProdList from "../context/jsons/ProdList.json"
 export default function Cards() {
   return (<>
     {ProdList.map((element, index) => {
-       return(
-         <>
-        <div class="card cards-prod">
-          <img src={element.image_url} class="card-img-top" alt="dress" />
-          <div class="card-body">
-            <p class="card-text">{element.description}</p>
+      return (
+
+        <div className="card cards-prod" key={index}>
+          <img src={element.image_url} className="card-img-top" alt="dress" />
+          <div className="card-body">
+            <p className="card-text">{element.description}</p>
           </div>
         </div>
-        </>
-       )    
+      )
     })}
   </>
   )
