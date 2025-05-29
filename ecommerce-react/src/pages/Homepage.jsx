d7862cd380ce569911d24334b59b8567eb7fcb66
MarcoVacchi <mvacchi816@gmail.com>
2025-05-29 11:15:16 +0200

    Feat: added social component in homepage

--- ecommerce-react/src/pages/Homepage.jsx
+++ ecommerce-react/src/pages/Homepage.jsx
@@ -3,13 +3,14 @@ import Sliders from "../components/Sliders";
 import HeroCard from "../components/HeroCard";
 import CardSections from "../components-layouts/CardSections";
 import { HomeProductSection } from "../components/HomeProductSection";
+import Social from '../components/Social';
 
 export default function Homepage() {
   return (
     <div>
       <Sliders />
       <HomeProductSection />
-
+      <Social />
       <HeroCard />
     </div>
   );
