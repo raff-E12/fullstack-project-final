668d9e794dd1b12102b14dd0d78d1996bbcd1e08
willymariino <wg.mariino@gmail.com>
2025-05-29 11:43:36 +0200

    fixed postion relative

--- ecommerce-react/src/components/HeroCard.jsx
+++ ecommerce-react/src/components/HeroCard.jsx
@@ -22,19 +22,19 @@ function HeroCard() {
 
         <div className="row py-5">
           <div className="col-md-6">
-            <div className="placeholder-img shadow rounded h-100 position relative" >
+            <div className="placeholder-img shadow rounded h-100 position-relative" >
               <button className="btn btn-light position absolute bottom-0 start-0 m-3">dressses</button>
             </div>
           </div>
           <div className="col-md-6">
             <div className="row h-100">
               <div className="col-12 mb-3">
-                <div className="placeholder-img rounded position relative" >
+                <div className="placeholder-img rounded position-relative" >
                   <button className="btn btn-light position absolute bottom-0 start-0 m-3">watches</button>
                 </div>
               </div>
               <div className="col-12">
-                <div className="placeholder-img rounded position relative" >
+                <div className="placeholder-img rounded position-relative" >
                   <button className="btn btn-light position absolute bottom-0 start-0 m-3">sneakers</button>
                 </div>
               </div>
