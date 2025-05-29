b409642527da022f3d1d01711cb9c91120f8a1ae
Raff_12 <raffelecoppola381@gmail.com>
2025-05-29 11:42:27 +0200

    feat: Modifica Progetto...

--- ecommerce-react/src/pages/ProductsPage.jsx
+++ ecommerce-react/src/pages/ProductsPage.jsx
@@ -29,26 +29,17 @@ export default function ProductPage() {
         getProducts();
         setSearchActive(true);
 
-<<<<<<< HEAD
          return () => {
       setSearchActive(false);
       setSearchBarActive(false);
     };
     }, []);
-=======
-        return () => {
-            setSearchActive(false);
-            setSearchBarActive(false);
-        };
-    }, [searchTerm]);
->>>>>>> 63a7e938654f26c1c4227a11d99b9a7b7f4311a2
 
     function handleSubmit(event) {
         event.preventDefault();
         getProducts();
     }
 
-<<<<<<< HEAD
     return(<>
     <div className="container-xl container-prod">
             <form className ={`container-xl form-prod`} onSubmit={handleSubmit}>
@@ -79,30 +70,6 @@ export default function ProductPage() {
                 </>)
             })}
         </div>
-=======
-    return <div>
-        <form className={isSearchBarActive ? "d-flex my-5 mx-auto" : "d-none"} onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Search by brand, name, or category"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <button type="submit">Search</button>
-        </form>
-
-        {products.map((product) => (
-            <div key={product.id}>
-                <Link to={`/products/${product.slug}`}>
-                    <h2>{product.name}</h2>
-                    <p>{product.description}</p>
-                    <p>Prezzo: €{product.price}</p>
-                    <img className="img-product" src={product.image_url} alt={product.name} />
-                </Link>
-                <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
-            </div>
-        ))}
->>>>>>> 63a7e938654f26c1c4227a11d99b9a7b7f4311a2
     </div>
     </>)
 }
