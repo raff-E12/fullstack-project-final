7055b7137b4e573050c0ea0163e47cbe59107c88
willymariino <wg.mariino@gmail.com>
2025-05-29 12:10:15 +0200

    feat: added responsiveness

--- ecommerce-react/src/components/HeroCard.jsx
+++ ecommerce-react/src/components/HeroCard.jsx
@@ -5,12 +5,12 @@ function HeroCard() {
     <section>
       <div className="container mt-5">
         <div className="row">
-          <div className="col-6">
+          <div className=" col-12 col-md-6">
             <h1 className="display-4 fw-bold text-center my-5">
               Le nostre collezioni
             </h1>
           </div>
-          <div className="col-6">
+          <div className="col-12 col-md-6">
             <p className="lead text-center my-5 text-justify">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
               itaque consequuntur culpa, obcaecati qui, at est pariatur, ipsa
@@ -20,13 +20,13 @@ function HeroCard() {
           </div>
         </div>
 
-        <div className="row py-5">
-          <div className="col-md-6">
+        <div className="row g-4 py-4">
+          <div className="col-12 col-md-6">
             <div className="placeholder-img shadow rounded h-100 position-relative" >
               <button className="btn btn-light position-absolute bottom-0 start-0 m-3">dressses</button>
             </div>
           </div>
-          <div className="col-md-6">
+          <div className="col-12 col-md-6">
             <div className="row h-100">
               <div className="col-12 mb-3">
                 <div className="placeholder-img rounded position-relative" >
