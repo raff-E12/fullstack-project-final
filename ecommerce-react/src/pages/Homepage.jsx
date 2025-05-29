870d56a1207493f7d82ef489777d554074e70d93
martinolanza03 <lanzamartino3@gmail.com>
2025-05-29 11:12:32 +0200

    feat: accept homepage

--- ecommerce-react/src/pages/Homepage.jsx
+++ ecommerce-react/src/pages/Homepage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Sliders from "../components/Sliders";
 import HeroCard from "../components/HeroCard";
@@ -14,21 +13,4 @@ export default function Homepage() {
       <HeroCard />
     </div>
   );
-=======
-import React from 'react'
-import Sliders from '../components/Sliders'
-import CardSections from '../components-layouts/CardSections'
-import Social from '../components/Social'
-
-export default function Homepage() {
-  return (
-    <>
-      <div className='container-xxl p-0'>
-        <Sliders />
-      </div>
-      <CardSections />
-      <Social />
-    </>
-  )
->>>>>>> ProductPage
 }
