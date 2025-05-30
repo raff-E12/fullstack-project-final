59724c13c96c50ebaba18fa45655a4edb3d055f2
MarcoVacchi <mvacchi816@gmail.com>
2025-05-30 11:55:09 +0200

    feat: added discount in singleProductPage

--- ecommerce-react/src/pages/SingleProductPage.jsx
+++ ecommerce-react/src/pages/SingleProductPage.jsx
@@ -28,33 +28,46 @@ export default function SingleProductPage() {
         getProductsSlug();
     }, []);
 
-    const { name, description, price, image_url, fabric, discount, category_name } = productSlug;
+    const { name, description, price, image_url, fabric, discount, category_name, start_discount, end_discount } = productSlug;
 
-    return(<>
-      <div className='container-xxl prod-sc d-flex'>
-       {productSlug.length !== 0 ?  <div className='container-lg p-2 d-flex sections-prod'>
-             <div className='prod-review'>
-                <p className='sale-prod'>{discount} %</p>
-                <div className='img-prod-add'>
-                    <img  src={image_url} alt={name} />
+    const today = new Date();
+
+    const start = start_discount ? new Date(start_discount) : null;
+    const end = end_discount ? new Date(end_discount) : null;
+
+    const isDiscountActive =
+        discount &&
+        start instanceof Date &&
+        end instanceof Date &&
+        today >= start &&
+        today <= end;
+
+
+    return (<>
+        <div className='container-xxl prod-sc d-flex'>
+            {productSlug.length !== 0 ? <div className='container-lg p-2 d-flex sections-prod'>
+                <div className='prod-review'>
+                    {isDiscountActive && <p className='sale-prod'>{discount} %</p>}
+                    <div className='img-prod-add'>
+                        <img src={image_url} alt={name} />
+                    </div>
                 </div>
-             </div>
-            <div className='prod-descriptions'>
-               <div className='text-prod'>
-                  <p id='text-dec'>- Descrizione Prodotti</p>
-                    <h1>{name}</h1>
-                    <h4 id='price'>€{price}</h4>
-               </div>
-               <div className='extra-prod'>
-                    <h5>Descrizione:</h5>
-                    <p>{fabric}</p>
-                    <p>{category_name}</p>
-                    <p>{description}</p>
-               </div>
-                <button className='btn-prod' onClick={() => addToCart(productSlug)}> Aggiungi al carrello </button>
-            </div>
-        </div> :  <div className='container-lg p-2 d-flex sections-prod'> <b>Loading...</b> </div>}
-      </div>
+                <div className='prod-descriptions'>
+                    <div className='text-prod'>
+                        <p id='text-dec'>- Descrizione Prodotti</p>
+                        <h1>{name}</h1>
+                        <h4 id='price'>€{price}</h4>
+                    </div>
+                    <div className='extra-prod'>
+                        <h5>Descrizione:</h5>
+                        <p>{fabric}</p>
+                        <p>{category_name}</p>
+                        <p>{description}</p>
+                    </div>
+                    <button className='btn-prod' onClick={() => addToCart(productSlug)}> Aggiungi al carrello </button>
+                </div>
+            </div> : <div className='container-lg p-2 d-flex sections-prod'> <b>Loading...</b> </div>}
+        </div>
     </>)
 
 };
