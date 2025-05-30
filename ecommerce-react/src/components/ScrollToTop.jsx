6f9ca5f38ecfc6120b6fbabaa88b6e567465dfaf
simoneburrai <burrai.s98@gmail.com>
2025-05-30 23:51:37 +0200

    feat:smoothed scrolling using behavior

--- ecommerce-react/src/components/ScrollToTop.jsx
+++ ecommerce-react/src/components/ScrollToTop.jsx
@@ -5,10 +5,12 @@ function ScrollToTop() {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0); // Scorre la finestra in cima
-  }, [location]); // Dipendenza da 'pathname': l'effetto si attiva ogni volta che il pathname cambia
-
-  return null; // Questo componente non renderizza nulla visivamente
+    window.scrollTo({
+  top: 0,
+  behavior: 'smooth' 
+});
+  }, [location]); 
+  return null; 
 }
 
 export default ScrollToTop;
