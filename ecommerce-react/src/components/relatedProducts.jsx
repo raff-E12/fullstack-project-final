58824500f1541dbf4ba2ffdb96bafbfa4ea9e4c0
willymariino <wg.mariino@gmail.com>
2025-06-02 20:28:21 +0200

    imported link

--- ecommerce-react/src/components/relatedProducts.jsx
+++ ecommerce-react/src/components/relatedProducts.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const RelatedProducts = ({ category }) => {
     const [relatedProducts, setRelatedProducts] = useState([]);
