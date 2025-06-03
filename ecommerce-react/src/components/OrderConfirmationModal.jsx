30258d6b7496df216e3098f1ae546c179f2a5929
willymariino <wg.mariino@gmail.com>
2025-06-03 14:01:54 +0200

    created pop-up component

--- ecommerce-react/src/components/OrderConfirmationModal.jsx
+++ ecommerce-react/src/components/OrderConfirmationModal.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-
 function OrderConfirmationModal({ isOpen, onClose, orderCode }) {
     if (!isOpen) return null
 
     return (
         <div className="overlay">
-
-
-
+            <div className="modal">
+                <button className="close-btn" onClick={onClose}></button>
+                <h2>Ordine Completato!</h2>
+                <p>Il tuo codice ordine è: <strong>{orderCode}</strong></p>
+            </div>
         </div>
 
     )
