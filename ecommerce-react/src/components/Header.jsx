36e1bfcf54fe3d6c1c739f63d861e85c88a9a88b
AndreaCatapano <a.catapano96@gmail.com>
2025-06-03 23:32:49 +0200

    style: fixed header

--- ecommerce-react/src/components/Header.jsx
+++ ecommerce-react/src/components/Header.jsx
@@ -5,6 +5,7 @@ import "../style/Header.css";
 import CategoriesDropdown from "../utilities/CategoriesDropdown.jsx";
 import SearchComponent from "../utilities/handleSearchButtonClick.jsx";
 import CartDropdown from "../utilities/CartDropdown.jsx";
+import MobileCartModal from "../utilities/MobileCartModal.jsx";
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -59,88 +60,105 @@ export default function Header() {
 
         {/* Mobile Header */}
         <div className="d-flex d-md-none justify-content-between align-items-center">
-          <h1 className="m-0 text-white fw-bold text-uppercase" id="text-hd">
-            Nome
-          </h1>
-          <button
-            className="mobile-menu-btn btn p-2 d-flex flex-column justify-content-center align-items-center"
-            type="button"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-            aria-label="Toggle navigation"
-          >
-            <span
-              className={`hamburger-line ${isMobileMenuOpen ? "open" : ""}`}
-            ></span>
-            <span
-              className={`hamburger-line ${isMobileMenuOpen ? "open" : ""}`}
-            ></span>
-            <span
-              className={`hamburger-line ${isMobileMenuOpen ? "open" : ""}`}
-            ></span>
-          </button>
+          <div className="col-4 d-flex justify-content-start">
+            <button
+              className="mobile-menu-btn btn p-2 d-flex flex-column justify-content-center align-items-center"
+              type="button"
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              aria-label="Toggle navigation"
+            >
+              <span
+                className={`hamburger-line ${isMobileMenuOpen ? "open" : ""}`}
+              ></span>
+              <span
+                className={`hamburger-line ${isMobileMenuOpen ? "open" : ""}`}
+              ></span>
+              <span
+                className={`hamburger-line ${isMobileMenuOpen ? "open" : ""}`}
+              ></span>
+            </button>
+          </div>
+
+          <div className="col-4 d-flex justify-content-center">
+            <h1
+              className="m-0 text-white fw-bold text-uppercase text-center"
+              id="text-hd"
+            >
+              Nome
+            </h1>
+          </div>
+
+          <div className="col-4 d-flex justify-content-end">
+            <MobileCartModal isMobile={true} closeMenus={closeMenus} />
+          </div>
         </div>
 
         {/* Mobile Menu */}
         {isMobileMenuOpen && (
-          <div className="d-md-none mt-3 mobile-nav-menu text-start ps-3">
-            <NavLink to="/" className="nav-link d-block" onClick={closeMenus}>
+          <div className="d-md-none mt-3 mobile-nav-menu ps-0">
+            <NavLink
+              to="/"
+              className="nav-link d-block text-start ps-3"
+              onClick={closeMenus}
+            >
               Homepage
             </NavLink>
             <NavLink
               to="/products"
-              className="nav-link d-block"
+              className="nav-link d-block text-start ps-3"
               onClick={closeMenus}
             >
               Products
             </NavLink>
 
             <button
-              className="nav-link btn btn-link text-start p-0 d-block"
+              className="nav-link btn btn-link text-start ps-3 d-block w-100 border-0"
+              style={{ textAlign: "left" }}
               onClick={() => setIsMobileCategoriesOpen(!isMobileCategoriesOpen)}
             >
               Categories {isMobileCategoriesOpen ? "▲" : "▼"}
             </button>
 
             {isMobileCategoriesOpen && (
-              <div className="ms-3">
+              <div className="ps-0">
                 <NavLink
                   to="/categories/polo-&-t-shirt"
-                  className="nav-link d-block"
+                  className="nav-link d-block text-start ps-4"
                   onClick={closeMenus}
                 >
                   Polo & T-Shirt
                 </NavLink>
                 <NavLink
                   to="/categories/capispalla"
-                  className="nav-link d-block"
+                  className="nav-link d-block text-start ps-4"
                   onClick={closeMenus}
                 >
                   Capispalla
                 </NavLink>
                 <NavLink
                   to="/categories/felpe"
-                  className="nav-link d-block"
+                  className="nav-link d-block text-start ps-4"
                   onClick={closeMenus}
                 >
                   Felpe
                 </NavLink>
                 <NavLink
                   to="/categories/pantaloni"
-                  className="nav-link d-block"
+                  className="nav-link d-block text-start ps-4"
                   onClick={closeMenus}
                 >
                   Pantaloni
                 </NavLink>
                 <NavLink
                   to="/categories/scarpe"
-                  className="nav-link d-block"
+                  className="nav-link d-block text-start ps-4"
                   onClick={closeMenus}
                 >
                   Scarpe
                 </NavLink>
                 <NavLink
                   to="/categories/streetwear"
-                  className="nav-link d-block"
+                  className="nav-link d-block text-start ps-4"
                   onClick={closeMenus}
                 >
                   Streetwear
@@ -148,9 +166,8 @@ export default function Header() {
               </div>
             )}
 
-            <div className="mobile-search-and-cart mt-3">
+            <div className="mobile-search-and-cart mt-3 ps-3">
               <SearchComponent isMobile={true} closeMenus={closeMenus} />
-              <CartDropdown isMobile={true} closeMenus={closeMenus} />
             </div>
           </div>
         )}
