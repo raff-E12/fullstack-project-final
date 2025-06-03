53dbb1780d47ca8c9969392e844b1d4e47163ef5
willymariino <wg.mariino@gmail.com>
2025-06-03 13:55:00 +0200

    scaffolding component

---
+++ ecommerce-react/src/components/OrderConfirmationModal.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function OrderConfirmationModal({ isOpen, onClose, orderCode }) {
+    if (!isOpen) return null
+
+    return (
+        <div className="overlay">
+
+
+
+        </div>
+
+    )
+}
+
+export default OrderConfirmationModal
